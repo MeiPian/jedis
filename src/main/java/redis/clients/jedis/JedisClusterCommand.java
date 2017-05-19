df527560c35c4d33c020dabd5d59799d61a6fede
741691740 <741691740@qq.com>
2017-05-19 18:57:43 +0800

    mget  可用

--- src/main/java/redis/clients/jedis/JedisClusterCommand.java
+++ src/main/java/redis/clients/jedis/JedisClusterCommand.java
@@ -1,5 +1,12 @@
 package redis.clients.jedis;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import redis.clients.jedis.exceptions.JedisAskDataException;
 import redis.clients.jedis.exceptions.JedisClusterException;
 import redis.clients.jedis.exceptions.JedisClusterMaxRedirectionsException;
@@ -12,165 +19,224 @@ import redis.clients.util.SafeEncoder;
 
 public abstract class JedisClusterCommand<T> {
 
-  private static final String NO_DISPATCH_MESSAGE = "No way to dispatch this command to Redis Cluster.";
+	private static final String NO_DISPATCH_MESSAGE = "No way to dispatch this command to Redis Cluster.";
 
-  private JedisClusterConnectionHandler connectionHandler;
-  private int maxAttempts;
-  private ThreadLocal<Jedis> askConnection = new ThreadLocal<Jedis>();
+	private JedisClusterConnectionHandler connectionHandler;
+	private int maxAttempts;
+	private ThreadLocal<Jedis> askConnection = new ThreadLocal<Jedis>();
 
-  public JedisClusterCommand(JedisClusterConnectionHandler connectionHandler, int maxAttempts) {
-    this.connectionHandler = connectionHandler;
-    this.maxAttempts = maxAttempts;
-  }
+	public JedisClusterCommand(JedisClusterConnectionHandler connectionHandler, int maxAttempts) {
+		this.connectionHandler = connectionHandler;
+		this.maxAttempts = maxAttempts;
+	}
 
-  public abstract T execute(Jedis connection);
+	public abstract T execute(Jedis connection);
 
-  public T run(String key) {
-    if (key == null) {
-      throw new JedisClusterException(NO_DISPATCH_MESSAGE);
-    }
+	public T run(String key) {
+		if (key == null) {
+			throw new JedisClusterException(NO_DISPATCH_MESSAGE);
+		}
 
-    return runWithRetries(SafeEncoder.encode(key), this.maxAttempts, false, false);
-  }
+		return runWithRetries(SafeEncoder.encode(key), this.maxAttempts, false, false);
+	}
 
-  public T run(int keyCount, String... keys) {
-    if (keys == null || keys.length == 0) {
-      throw new JedisClusterException(NO_DISPATCH_MESSAGE);
-    }
+	public T run(int keyCount, String... keys) {
+		if (keys == null || keys.length == 0) {
+			throw new JedisClusterException(NO_DISPATCH_MESSAGE);
+		}
 
-    // For multiple keys, only execute if they all share the
-    // same connection slot.
-    if (keys.length > 1) {
-      int slot = JedisClusterCRC16.getSlot(keys[0]);
-      for (int i = 1; i < keyCount; i++) {
-        int nextSlot = JedisClusterCRC16.getSlot(keys[i]);
-        if (slot != nextSlot) {
-          throw new JedisClusterException("No way to dispatch this command to Redis Cluster "
-              + "because keys have different slots.");
-        }
-      }
-    }
+		// For multiple keys, only execute if they all share the
+		// same connection slot.
+		if (keys.length > 1) {
+			List<String> result = new ArrayList<>();
+			Map<JedisPool, List<String>> poolKeysMap = getPoolKeyMap(Arrays.asList(keys));
+			for (Map.Entry<JedisPool, List<String>> entry : poolKeysMap.entrySet()) {
+				JedisPool pool = entry.getKey();
+				List<String> subkeys = entry.getValue();
+				if (subkeys == null || subkeys.isEmpty()) {
+					continue;
+				}
+				Jedis jedis = null;
+				Pipeline pipeline = null;
+				List<Object> subResultList = new ArrayList<>();
+				try {
+					jedis = pool.getResource();
+					pipeline = jedis.pipelined();
+					for (String key : subkeys) {
+						pipeline.get(key);
+					}
+					subResultList = pipeline.syncAndReturnAll();
+				} catch (Exception e) {
+				} finally {
+					if (pipeline != null)
+						try {
+							pipeline.close();
+						} catch (Exception e) {
+							e.printStackTrace();
+						} finally {
+							if (jedis != null) {
+								jedis.close();
+							}
+						}
 
-    return runWithRetries(SafeEncoder.encode(keys[0]), this.maxAttempts, false, false);
-  }
+				}
+				for (Object object : subResultList) {
+					if (object != null) {
+						result.add(object.toString());
+					}
+				}
 
-  public T runBinary(byte[] key) {
-    if (key == null) {
-      throw new JedisClusterException(NO_DISPATCH_MESSAGE);
-    }
+			}
+			return (T) result;
+		}
 
-    return runWithRetries(key, this.maxAttempts, false, false);
-  }
+		return runWithRetries(SafeEncoder.encode(keys[0]), this.maxAttempts, false, false);
+	}
 
-  public T runBinary(int keyCount, byte[]... keys) {
-    if (keys == null || keys.length == 0) {
-      throw new JedisClusterException(NO_DISPATCH_MESSAGE);
-    }
+	public T runBinary(byte[] key) {
+		if (key == null) {
+			throw new JedisClusterException(NO_DISPATCH_MESSAGE);
+		}
 
-    // For multiple keys, only execute if they all share the
-    // same connection slot.
-    if (keys.length > 1) {
-      int slot = JedisClusterCRC16.getSlot(keys[0]);
-      for (int i = 1; i < keyCount; i++) {
-        int nextSlot = JedisClusterCRC16.getSlot(keys[i]);
-        if (slot != nextSlot) {
-          throw new JedisClusterException("No way to dispatch this command to Redis Cluster "
-              + "because keys have different slots.");
-        }
-      }
-    }
+		return runWithRetries(key, this.maxAttempts, false, false);
+	}
 
-    return runWithRetries(keys[0], this.maxAttempts, false, false);
-  }
+	public T runBinary(int keyCount, byte[]... keys) {
+		if (keys == null || keys.length == 0) {
+			throw new JedisClusterException(NO_DISPATCH_MESSAGE);
+		}
 
-  public T runWithAnyNode() {
-    Jedis connection = null;
-    try {
-      connection = connectionHandler.getConnection();
-      return execute(connection);
-    } catch (JedisConnectionException e) {
-      throw e;
-    } finally {
-      releaseConnection(connection);
-    }
-  }
+		// For multiple keys, only execute if they all share the
+		// same connection slot.
+		if (keys.length > 1) {
+			int slot = JedisClusterCRC16.getSlot(keys[0]);
+			for (int i = 1; i < keyCount; i++) {
+				int nextSlot = JedisClusterCRC16.getSlot(keys[i]);
+				if (slot != nextSlot) {
+					throw new JedisClusterException(
+							"No way to dispatch this command to Redis Cluster " + "because keys have different slots.");
+				}
+			}
+		}
 
-  private T runWithRetries(byte[] key, int attempts, boolean tryRandomNode, boolean asking) {
-    if (attempts <= 0) {
-      throw new JedisClusterMaxRedirectionsException("Too many Cluster redirections?");
-    }
+		return runWithRetries(keys[0], this.maxAttempts, false, false);
+	}
 
-    Jedis connection = null;
-    try {
+	public T runWithAnyNode() {
+		Jedis connection = null;
+		try {
+			connection = connectionHandler.getConnection();
+			return execute(connection);
+		} catch (JedisConnectionException e) {
+			throw e;
+		} finally {
+			releaseConnection(connection);
+		}
+	}
 
-      if (asking) {
-        // TODO: Pipeline asking with the original command to make it
-        // faster....
-        connection = askConnection.get();
-        connection.asking();
+	private T runWithRetries(byte[] key, int attempts, boolean tryRandomNode, boolean asking) {
+		if (attempts <= 0) {
+			throw new JedisClusterMaxRedirectionsException("Too many Cluster redirections?");
+		}
 
-        // if asking success, reset asking flag
-        asking = false;
-      } else {
-        if (tryRandomNode) {
-          connection = connectionHandler.getConnection();
-        } else {
-          connection = connectionHandler.getConnectionFromSlot(JedisClusterCRC16.getSlot(key));
-        }
-      }
+		Jedis connection = null;
+		try {
 
-      return execute(connection);
+			if (asking) {
+				// TODO: Pipeline asking with the original command to make it
+				// faster....
+				connection = askConnection.get();
+				connection.asking();
 
-    } catch (JedisNoReachableClusterNodeException jnrcne) {
-      throw jnrcne;
-    } catch (JedisConnectionException jce) {
-      // release current connection before recursion
-      releaseConnection(connection);
-      connection = null;
+				// if asking success, reset asking flag
+				asking = false;
+			} else {
+				if (tryRandomNode) {
+					connection = connectionHandler.getConnection();
+				} else {
+					connection = connectionHandler.getConnectionFromSlot(JedisClusterCRC16.getSlot(key));
+				}
+			}
 
-      if (attempts <= 1) {
-        //We need this because if node is not reachable anymore - we need to finally initiate slots renewing,
-        //or we can stuck with cluster state without one node in opposite case.
-        //But now if maxAttempts = 1 or 2 we will do it too often. For each time-outed request.
-        //TODO make tracking of successful/unsuccessful operations for node - do renewing only
-        //if there were no successful responses from this node last few seconds
-        this.connectionHandler.renewSlotCache();
+			return execute(connection);
 
-        //no more redirections left, throw original exception, not JedisClusterMaxRedirectionsException, because it's not MOVED situation
-        throw jce;
-      }
+		} catch (JedisNoReachableClusterNodeException jnrcne) {
+			throw jnrcne;
+		} catch (JedisConnectionException jce) {
+			// release current connection before recursion
+			releaseConnection(connection);
+			connection = null;
 
-      return runWithRetries(key, attempts - 1, tryRandomNode, asking);
-    } catch (JedisRedirectionException jre) {
-      // if MOVED redirection occurred,
-      if (jre instanceof JedisMovedDataException) {
-        // it rebuilds cluster's slot cache
-        // recommended by Redis cluster specification
-        this.connectionHandler.renewSlotCache(connection);
-      }
+			if (attempts <= 1) {
+				// We need this because if node is not reachable anymore - we
+				// need to finally initiate slots renewing,
+				// or we can stuck with cluster state without one node in
+				// opposite case.
+				// But now if maxAttempts = 1 or 2 we will do it too often. For
+				// each time-outed request.
+				// TODO make tracking of successful/unsuccessful operations for
+				// node - do renewing only
+				// if there were no successful responses from this node last few
+				// seconds
+				this.connectionHandler.renewSlotCache();
 
-      // release current connection before recursion or renewing
-      releaseConnection(connection);
-      connection = null;
+				// no more redirections left, throw original exception, not
+				// JedisClusterMaxRedirectionsException, because it's not MOVED
+				// situation
+				throw jce;
+			}
 
-      if (jre instanceof JedisAskDataException) {
-        asking = true;
-        askConnection.set(this.connectionHandler.getConnectionFromNode(jre.getTargetNode()));
-      } else if (jre instanceof JedisMovedDataException) {
-      } else {
-        throw new JedisClusterException(jre);
-      }
+			return runWithRetries(key, attempts - 1, tryRandomNode, asking);
+		} catch (JedisRedirectionException jre) {
+			// if MOVED redirection occurred,
+			if (jre instanceof JedisMovedDataException) {
+				// it rebuilds cluster's slot cache
+				// recommended by Redis cluster specification
+				this.connectionHandler.renewSlotCache(connection);
+			}
 
-      return runWithRetries(key, attempts - 1, false, asking);
-    } finally {
-      releaseConnection(connection);
-    }
-  }
+			// release current connection before recursion or renewing
+			releaseConnection(connection);
+			connection = null;
 
-  private void releaseConnection(Jedis connection) {
-    if (connection != null) {
-      connection.close();
-    }
-  }
+			if (jre instanceof JedisAskDataException) {
+				asking = true;
+				askConnection.set(this.connectionHandler.getConnectionFromNode(jre.getTargetNode()));
+			} else if (jre instanceof JedisMovedDataException) {
+			} else {
+				throw new JedisClusterException(jre);
+			}
 
+			return runWithRetries(key, attempts - 1, false, asking);
+		} finally {
+			releaseConnection(connection);
+		}
+	}
+
+	private void releaseConnection(Jedis connection) {
+		if (connection != null) {
+			connection.close();
+		}
+	}
+
+	private Map<JedisPool, List<String>> getPoolKeyMap(List<String> keys) {
+		Map<JedisPool, List<String>> poolKeysMap = new LinkedHashMap<JedisPool, List<String>>();
+		try {
+			for (String key : keys) {
+				int slot = JedisClusterCRC16.getSlot(key);
+				JedisPool jedisPool = this.connectionHandler.getJedisPoolFromSlot(slot);
+				if (poolKeysMap.containsKey(jedisPool)) {
+					poolKeysMap.get(jedisPool).add(key);
+				} else {
+					List<String> subKeyList = new ArrayList<String>();
+					subKeyList.add(key);
+					poolKeysMap.put(jedisPool, subKeyList);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return poolKeysMap;
+	}
 }
